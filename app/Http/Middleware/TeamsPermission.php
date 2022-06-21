467a0e67f42fb1058896ab141ea8b7ac1a2b0e10
mohamadsyalvasr <33284353+mohamadsyalvasr@users.noreply.github.com>
2022-06-21 19:41:00 +0700

    add role and permission

---
+++ app/Http/Middleware/TeamsPermission.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class TeamsPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        if(!empty(auth()->user())){
+            // session value set on login
+            setPermissionsTeamId(session('team_id'));
+        }
+        // other custom ways to get team_id
+        /*if(!empty(auth('api')->user())){
+            // `getTeamIdFromToken()` example of custom method for getting the set team_id
+            setPermissionsTeamId(auth('api')->user()->getTeamIdFromToken());
+        }*/
+
+        return $next($request);
+    }
+}
