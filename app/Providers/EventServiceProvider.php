0044b3e073c26c422efde6d7f3a935cddf3a5851
mohamadsyalvasr <33284353+mohamadsyalvasr@users.noreply.github.com>
2022-06-27 21:33:14 +0700

    remove all event service

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,25 +6,6 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use Modules\Item\Entities\Item;
-use Modules\Item\Entities\ItemBrand;
-use Modules\Item\Entities\ItemHasCategory;
-use Modules\Item\Entities\ItemHasVariation;
-use Modules\Item\Entities\ItemIsPackage;
-use Modules\Item\Entities\ItemPricing;
-use Modules\Item\Entities\ItemUnit;
-use Modules\Item\Entities\ItemUnitConvention;
-use Modules\Item\Entities\ItemVariation;
-use Modules\Item\Entities\ItemWarranty;
-use Modules\Item\Observers\ItemBrandObserver;
-use Modules\Item\Observers\ItemHasCategoryObserver;
-use Modules\Item\Observers\ItemHasVariationObserver;
-use Modules\Item\Observers\ItemIsPackageObserver;
-use Modules\Item\Observers\ItemObserver;
-use Modules\Item\Observers\ItemPricingObserver;
-use Modules\Item\Observers\ItemUnitConventionObserver;
-use Modules\Item\Observers\ItemUnitObserver;
-use Modules\Item\Observers\ItemWarrantyObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -47,16 +28,6 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Item::observe(ItemObserver::class);
-        ItemBrand::observe(ItemBrandObserver::class);
-        ItemUnit::observe(ItemUnitObserver::class);
-        ItemUnitConvention::observe(ItemUnitConventionObserver::class);
-        ItemVariation::observe(ItemUnitConventionObserver::class);
-        ItemWarranty::observe(ItemWarrantyObserver::class);
-        ItemHasVariation::observe(ItemHasVariationObserver::class);
-        ItemHasCategory::observe(ItemHasCategoryObserver::class);
-        ItemPricing::observe(ItemPricingObserver::class);
-        ItemIsPackage::observe(ItemIsPackageObserver::class);
     }
 
     /**
